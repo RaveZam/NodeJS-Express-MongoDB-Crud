bb52e5ff4343b6831a19ac64d4f0b2bc20a65ca1
Duke <114975455+RaveZam@users.noreply.github.com>
2024-12-21 13:52:57 +0800

    commit

--- index.js
+++ index.js
@@ -8,10 +8,66 @@ app.get("/", (req, res) => {
   res.send("Push from NodeJS Update");
 });
 
+//Get all
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by ID
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update Item
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
+    }
+
+    const UpdatedProduct = await Product.findById(id);
+    res.status(200).json(UpdatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Post Item
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete Item
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+
+    res.status(200).json({ message: "Product Deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
